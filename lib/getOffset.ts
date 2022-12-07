fa172fa73166af475c3f589cadda5e3dd365834b
Cristiana Chavez <cristianachavez214@gmail.com>
2022-12-07 19:34:30 +0800

    Added git workflows

---
+++ lib/getOffset.ts
@@ -0,0 +1,8 @@
+const getOffset = (pageNum: string, pageLimit?:number) => {
+    const limit = pageLimit || 25;
+    const offset = (parseInt(pageNum, 10) - 1) * limit;
+    return offset;
+  };
+  
+  export default getOffset;
+  
