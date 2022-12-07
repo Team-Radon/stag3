21750944b7cecf8ac12bc0210efbd2bac77bcd29
Cristiana Chavez <cristianachavez214@gmail.com>
2022-12-07 19:43:10 +0800

    Update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'standard-with-typescript',
-    "next/core-web-vitals"
+    'next/core-web-vitals'
   ],
   overrides: [
   ],
@@ -19,23 +19,5 @@ module.exports = {
     'react'
   ],
   rules: {
-    "no-undef": "off",
-		"no-tabs": "off",
-		"no-use-before-define": "off",
-		"react/function-component-definition": "off",
-		"react/react-in-jsx-scope": "off",
-		"react/jsx-filename-extension": "off",
-		"react/jsx-props-no-spreading": "off",
-		"react/require-default-props": "off",
-		"react/button-has-type": "off",
-		"import/extensions": "off",
-		"import/prefer-default-export": "off",
-		"no-console": "off",
-		"max-len": "off",
-		"import/order": "error",
-		"import/no-extraneous-dependencies": "off",
-		"quote-props": "off",
-		"jsx-a11y/anchor-is-valid": "off",
-		"import/no-cycle": "off"
   }
 }
