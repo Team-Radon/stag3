501e603e56e233bfa82042fee819e1edf3d59454
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 <cristianachavez214@gmail.com>
2022-12-08 16:22:23 +0800

    update eslint config

--- lib/getOffset.ts
+++ lib/getOffset.ts
@@ -1,6 +1,6 @@
-const getOffset = (pageNum: string, pageLimit?:number) => {
+function getOffset(pageNum: string, pageLimit?: number) {
     const limit = pageLimit || 25;
     const offset = (parseInt(pageNum, 10) - 1) * limit;
     return offset;
-  };
+}
 export default getOffset;
