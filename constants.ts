210c5ae098193ba99e8657ace50aa50b1fa49ff5
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 <cristianachavez214@gmail.com>
2022-12-13 22:16:16 +0800

    feat(tags)  update tags to label and value default in select

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@
 export const IFRAMELY_API_KEY =
   process.env.IFRAMELY_API_KEY ?? 'b357c70e91259962333082';
 // list of tags
-export const tags = [{ name: 'Privacy', url: 'privacy' }, { name: 'DeFi', url: 'defi' }, { name: 'Marketplace', url: 'marketplace' }, { name: 'Wallet', url: 'wallet' }, { name: 'DAO', url: 'dao' }, { name: 'Social', url: 'social' }, { name: 'Gaming', url: 'gaming' }, { name: 'Metaverse', url: 'metaverse' }, { name: 'NFT', url: 'nft' }, { name: 'Tool', url: 'tool' }, { name: 'Analytics', url: 'analytics' }, { name: 'Content', url: 'content' }, { name: 'Other', url: 'other' }];
+export const tags = [{ label: 'Privacy', value: 'privacy' }, { label: 'DeFi', value: 'defi' }, { label: 'Marketplace', value: 'marketplace' }, { label: 'Wallet', value: 'wallet' }, { label: 'DAO', value: 'dao' }, { label: 'Social', value: 'social' }, { label: 'Gaming', value: 'gaming' }, { label: 'Metaverse', value: 'metaverse' }, { label: 'NFT', value: 'nft' }, { label: 'Tool', value: 'tool' }, { label: 'Analytics', value: 'analytics' }, { label: 'Content', value: 'content' }, { label: 'Other', value: 'other' }];
