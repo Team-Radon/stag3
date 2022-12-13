f3822baec19b079f35f7c019fef0b4db6a553165
ayad-nadeeb <nadeeb.ayad@gmail.com>
2022-12-13 19:23:05 +0800

    style: update branding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { Button } from './UI/Button'
 import { NavBarAccount } from './NavBarAccount';
 
 const NavBar = () => {
   const { push } = useRouter();
   return (
-    <div className="sticky top-0 z-10 flex shrink-0 justify-between items-center w-full h-16 sm:h-auto px-4 sm:p-7 backdrop-blur">
-      <div className="relative w-32">
-        <Image src="https://i.imgur.com/Skr4I1Z.png" alt="brand" width={100} height={100} unoptimized />
-      </div>
+    <div className="sticky top-0 z-10 flex shrink-0 justify-between items-center w-full h-16 sm:h-auto p-4 sm:p-6 backdrop-blur">
+      <Link href="/" title="Home">
+        <div className="brand flex items-center gap-2">
+          <div className="relative w-10">
+            <Image src="./stag3.svg" alt="brand" width={50} height={50} unoptimized />
+          </div>
+          <div className="text-4xl font-extrabold tracking-tight text-slate-800">stage</div>
+        </div>
+      </Link>
       <div className="flex flex-row gap-5 items-center">
         <div className="hidden md:flex h-8 items-center gap-3 border-r border-gray-300 pr-5">
           <Button icon={<PlusCircleIcon className="w-6 h-6" />} onClick={async () => push('/add')}>
