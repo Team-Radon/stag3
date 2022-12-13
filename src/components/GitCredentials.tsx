2ce8228720cc315a930f6202102346f881925a3c
ayad-nadeeb <nadeeb.ayad@gmail.com>
2022-12-13 19:20:31 +0800

    style: update layout for verified credentials

--- src/components/GitCredentials.tsx
+++ src/components/GitCredentials.tsx
@@ -7,6 +7,7 @@ import { useAddress } from '../hooks/useAddress';
 import { useAppStore } from '../store/useAppStore';
 
 import { Button } from './UI/Button';
+import Card from './UI/Card';
 
 export const GitCredentials = () => {
   const reader = new PassportReader();
@@ -25,6 +26,8 @@ export const GitCredentials = () => {
     }
   }
 
+  console.log(passport)
+
   useEffect(() => {
     if (address) {
       readGitCredentials()
@@ -37,47 +40,63 @@ export const GitCredentials = () => {
       )
     : user
       ? (
-        <div className="mt-3 px-4 py-6 sm:px-6 shadow bg-white rounded-lg md:rounded-xl">
-          <div className="text-xl font-medium mb-2">Gitcoin Verified Crendentials</div>
-
-          <span className="text-sm">
-            {address}
-          </span>
-          {passport?.stamps?.length
-            ? (
-              <div className=" flex flex-row flex-wrap px-5">
-                {
-              passport?.stamps?.map((item, index) => (
-                <div
-                  className="rounded flex flex-col items-center justify-center mr-2 mb-2 px-2 py-4 relative;
-                  width: 31%"
-                  key={index}
-                >
-                  <span className="flex flex-row items-center text-sm text-center break-all">
-                    {item.provider}
-                  </span>
-                  <a href="https://passport.gitcoin.co/" target="_blank" rel="noreferrer">
-                    <div className="chips inline-flex px-4 py-1 rounded-full bg-gray-200 text-xs ">
-                      <img src="https://app.orbis.club/img/icons/gitcoin-logo.png" className="mright-5" height={18} width={18} />
-                      <span>Gitcoin Passport</span>
-                    </div>
+        <Card>
+          <div>
+            <div className="space-y-2 border-b border-gray-200 pb-2">
+              <div className="flex items-center h-8">
+                <img src="https://passport.gitcoin.co/assets/gitcoinLogoDark.svg" alt="Gitcoin Logo" />
+                <img className="mx-4 h-6" src="https://passport.gitcoin.co/assets/logoLine.svg" alt="Logo Line" />
+                <img src="https://passport.gitcoin.co/assets/passportLogoBlack.svg" alt="pPassport Logo" />
+              </div>
+              <div className="text-sm truncate">{address}</div>
+            </div>
+            {passport?.stamps?.length
+              ? (
+                <div>
+                  <div className="grid grid-flow-row grid-cols-2 gap-4 mt-4">
+                    {passport?.stamps?.map((item, index) => (
+                      <div key={index} className="px-4 py-2 rounded-lg bg-gray-100">
+                        <div className="flex items-center gap-4">
+                          <div className="min-w-0 truncate flex-1 text-sm font-medium text-gray-900">
+                            {item.provider}
+                          </div>
+                          <svg className="w-5 h-5 fill-emerald-600" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                          </svg>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <a
+                    className="flex justify-center items-center mt-4 border border-skin-border font-medium text-skin-text hover:text-accent hover:border-accent px-3 py-2 text-sm space-x-2 rounded-lg"
+                    href="https://passport.gitcoin.co/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span>View Stamps</span>
+                    <svg className="w-5 h-5" fill="currentColor" stroke="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                    </svg>
                   </a>
                 </div>
-              ))
-}
-
-              </div>
-              )
-            : (
-              <a href="https://passport.gitcoin.co/" target="_blank" rel="noreferrer">
-                <div className="bg-gray-200 flex items-center font-normal text-xs py-1 px-2 rounded-full">
-                  <img src="https://app.orbis.club/img/icons/gitcoin-logo.png" className="mright-5" height={18} width={18} />
-                  <span className="items-center font-normal text-xs py-1 px-2">Connect Gitcoin Passport</span>
-                </div>
-              </a>
-              )}
-        </div>
-
+                )
+              : (
+                <a
+                  className="flex justify-center items-center mt-4 border border-skin-border font-medium text-skin-text hover:text-accent hover:border-accent px-3 py-2 text-sm space-x-2 rounded-lg"
+                  href="https://passport.gitcoin.co/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span>Connect Gitcoin Passport</span>
+                  <svg className="w-5 h-5" fill="currentColor" stroke="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                  </svg>
+                </a>
+                )}
+          </div>
+        </Card>
         )
       : null
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,16 @@ const Home = () => (
   <SiteLayout>
     <main className="container mx-auto px-4 md:px-6 2xl:px-20">
       <Hero />
-      <div className="space-y-10 lg:flex  lg:gap-20 lg:space-y-0">
+      <div className="space-y-10 lg:flex lg:gap-20 lg:space-y-0">
         <ul className="list space-y-4 md:space-y-6 lg:w-2/3">
           <ProductItem />
           <ProductItem />
           <ProductItem />
           <ProductItem />
         </ul>
-        <div className="sidebar lg:w-1/3">
-          <ActiveUsers />
+        <div className="sidebar space-y-4 md:space-y-6 lg:w-1/3">
           <GitCredentials />
+          <ActiveUsers />
         </div>
       </div>
     </main>
