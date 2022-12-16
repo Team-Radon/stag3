65e603f8835cccf8f93175a03bf5d1b9ddc7f3dc
Al Joseph Condino <condinoaljoseph@gmail.com>
2022-12-16 23:09:29 +0800

    feat(comment): add two level comments

--- src/components/CommentsItem.tsx
+++ src/components/CommentsItem.tsx
@@ -1,34 +1,70 @@
 import { Comment } from '@/helpers/interfaces';
+import { useGetComments } from '@/orbis/useGetComments';
 import { ChatBubbleLeftIcon, HandThumbUpIcon } from '@heroicons/react/24/outline';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { Dispatch, SetStateAction } from 'react';
 import { AvatarUser } from './AvatarUser';
+import { CommentForm } from './CommentForm';
 import Card from './UI/Card';
 
 dayjs.extend(relativeTime);
 
-export const CommentsItem = ({ comment }: { comment: Comment }) => (
-  <Card className="border-none">
-    <div className="mb-2 flex items-center justify-between">
-      <div className="flex items-center space-x-1">
-        <AvatarUser size="28" details={comment.creator_details} />
+interface CommentsItemProps {
+  comment: Comment
+  master?: string | null
+  activeComment: { id: string, type: 'replying' | 'editing' } | null
+  setActiveComment: Dispatch<SetStateAction<{ id: string, type: 'replying' | 'editing' } | null>>
+  addComment: (body: string, master?: string) => void
+}
+
+export const CommentsItem = ({ comment, master = null, activeComment, setActiveComment, addComment }: CommentsItemProps) => {
+  const { data: replies } = useGetComments({ id: comment.stream_id });
+  const isReplying = activeComment && activeComment.type === 'replying' && activeComment.id === comment.stream_id
+  const replyId = master || comment.stream_id
+
+  return (
+    <Card className="border-none">
+      <div className="mb-2 flex items-center justify-between">
+        <div className="flex items-center space-x-1">
+          <AvatarUser size="28" details={comment.creator_details} />
+        </div>
+        <span>{dayjs(new Date(comment.timestamp * 1000)).fromNow()}</span>
       </div>
-      <span>{dayjs(new Date(comment.timestamp * 1000)).fromNow()}</span>
-    </div>
-    <div className="relative mb-1 break-words pr-[80px] leading-7">
-      <p className="mb-2 break-words text-md line-clamp-2">
-        {comment.content.body}
-      </p>
-    </div>
-    <div className="flex space-x-3">
-      <span className="flex items-center space-x-1">
-        <ChatBubbleLeftIcon className="w-4 h-4" />
-        {comment.count_replies ?? 0}
-      </span>
-      <span className="flex items-center space-x-2">
-        <HandThumbUpIcon className="w-4 h-4" />
-        {comment.count_likes ?? 0}
-      </span>
-    </div>
-  </Card>
-);
+      <div className="relative mb-1 break-words pr-[80px] leading-7">
+        <p className="mb-2 break-words text-md line-clamp-2">
+          {comment.content.body}
+        </p>
+      </div>
+      <div className="flex space-x-3">
+        <span className="flex items-center space-x-1">
+          <ChatBubbleLeftIcon className="w-4 h-4" />
+          {comment.count_replies ?? 0}
+        </span>
+        <span className="flex items-center space-x-2">
+          <HandThumbUpIcon className="w-4 h-4" />
+          {comment.count_likes ?? 0}
+        </span>
+        <span className="flex items-center space-x-2" onClick={() => setActiveComment({ id: comment.stream_id, type: 'replying' })}>
+          <HandThumbUpIcon className="w-4 h-4" />
+          Reply
+        </span>
+      </div>
+      {isReplying && <CommentForm handleSubmit={(text) => addComment(text, replyId)} />}
+      {replies && replies?.data?.length > 0 && (
+        <div className="p-4">
+          {replies?.data?.map((reply) => (
+            <CommentsItem
+              key={reply.stream_id}
+              master={comment?.stream_id}
+              addComment={addComment}
+              comment={reply}
+              activeComment={activeComment}
+              setActiveComment={setActiveComment}
+            />
+          ))}
+        </div>
+      )}
+    </Card>
+  )
+};
