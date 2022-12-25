4cd09a89835ae0a8cd1c444f71f7dbd9845d94d9
ayad-nadeeb <nadeeb.ayad@gmail.com>
2022-12-25 16:45:18 +0800

    minor project page layout adjustment

--- src/pages/[stream_id]/index.tsx
+++ src/pages/[stream_id]/index.tsx
@@ -40,16 +40,17 @@ const Project = () => {
           )
         : (
           <>
-            <GridItemEight className="space-y-4">
+            <GridItemEight>
               <Card>
+                {/* cover image */}
                 {post?.data?.content?.data?.cover && (
-                <div className="cover mb-6">
-                  <div className="relative aspect-[5/2] md:aspect-[3/1] overflow-hidden rounded">
+                  <div className="relative aspect-[5/2] md:aspect-[3/1] overflow-hidden">
                     <Image src={post?.data?.content?.data?.cover} alt="cover" fill style={{ objectFit: 'cover', objectPosition: 'top' }} />
                   </div>
-                </div>
                 )}
-                <div className="flex flex-col md:flex-row justify-between items-start -mt-20 mb-6 md:mb-8 md:mt-0">
+
+                {/* header */}
+                <div className="flex flex-col md:flex-row justify-between items-start px-4 md:px-6 -mt-16 md:mt-12">
                   <div className="flex flex-col items-center md:flex-row gap-4 md:gap-6">
                     <div className="logo shrink-0 relative aspect-square w-24 rounded-md md:rounded-lg overflow-hidden">
                       <Image
@@ -62,7 +63,7 @@ const Project = () => {
                       />
                     </div>
                     <div className="px-4 md:px-0">
-                      <div className="text-center md:text-left">
+                      <div className="text-center md:text-left text-black">
                         <div className="text-xs font-medium tracking-wider uppercase mb-2">Status</div>
                         <h1 className="text-2xl mb-2">{post?.data?.content?.title}</h1>
                         <div className="content space-y-6 text-sm break-words">
@@ -75,30 +76,33 @@ const Project = () => {
                     <ButtonReaction creator={post?.data?.creator || ''} stream_id={post?.data?.stream_id || ''} count_downvotes={post?.data?.count_downvotes || 0} count_likes={post?.data?.count_haha || 0} />
                   </div>
                 </div>
-                <div className="font-medium leading-6 text-gray-900 border-b border-gray-200 pb-2">Details</div>
-                <div className="about mt-6">
-                  <div className="text-sm font-medium text-gray-500 mb-4">Tags</div>
-                  <div className="tag-chips flex flex-wrap items-center justify-center md:justify-start gap-3 mt-6">
-                    {post?.data?.content?.tags.map(({ title, slug }) => (
-                      <Link key={slug} href={`/list/${slug}`}>
-                        <span className="bg-indigo-100 border border-indigo-300 hover:bg-indigo-200 text-indigo-600 text-xs font-semibold px-2.5 py-0.5 rounded-md dark:bg-blue-200 dark:text-indigo-800 dark:hover:bg-indigo-300">
-                          {title}
-                        </span>
-                      </Link>
-                    ))}
-                  </div>
+
+                {/* section label */}
+                <div className="font-medium leading-6 text-black border-b border-gray-200 px-4 pb-2 md:px-6 mt-4 md:mt-12">Details</div>
+
+                {/* tags */}
+                <div className="tag-chips flex flex-wrap items-center gap-3 px-4 md:px-6 mt-6">
+                  {post?.data?.content?.tags.map(({ title, slug }) => (
+                    <Link key={slug} href={`/list/${slug}`}>
+                      <span className="bg-indigo-100 border border-indigo-300 hover:bg-indigo-200 text-indigo-600 text-xs font-semibold px-2.5 py-0.5 rounded-md dark:bg-blue-200 dark:text-indigo-800 dark:hover:bg-indigo-300">
+                        {title}
+                      </span>
+                    </Link>
+                  ))}
                 </div>
-                <div className="about mt-6">
-                  <div className="text-sm font-medium text-gray-500 mb-4">About</div>
-                  <div className="prose prose-sm max-w-none">
-                    <Markdown source={post?.data?.content?.data?.description_long} />
-                  </div>
+
+                {/* about */}
+                <div className="about prose-sm max-w-none text-black px-4 md:px-6 mt-4">
+                  <Markdown source={post?.data?.content?.data?.description_long} />
                 </div>
-                <div className="font-medium leading-6 text-gray-900 border-b border-gray-200 pb-2">Links</div>
-                <div className="links flex flex-col md:flex-row md:gap-20">
-                  <div className="official-sites w-1/2 mt-6">
-                    <div className="text-sm font-medium text-gray-500 mb-4">Official</div>
-                    <Link href={post?.data?.content?.data?.website ?? ''} className="group flex items-center gap-1 mt-2 text-sm text-sky-600 hover:underline">
+
+                {/* section label */}
+                <div className="font-medium leading-6 text-black border-b border-gray-200 px-4 pb-2 md:px-6 mt-12">Links</div>
+
+                {/* links */}
+                <div className="links flex flex-col md:flex-row md:gap-20 px-4 md:px-6 mt-6">
+                  <div className="official-sites">
+                    <Link href={post?.data?.content?.data?.website ?? ''} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
                       <span>Website</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
@@ -115,9 +119,8 @@ const Project = () => {
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
                   </div>
-                  <div className="socials w-1/2 mt-6">
-                    <div className="text-sm font-medium text-gray-500 mb-4">Socials</div>
-                    <Link href={post?.data?.content?.data?.discord ?? ''} className="group flex items-center gap-1 mt-2 text-sm text-sky-600 hover:underline">
+                  <div className="socials">
+                    <Link href={post?.data?.content?.data?.discord ?? ''} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
                       <span>Discord</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
@@ -127,8 +130,8 @@ const Project = () => {
                     </Link>
                   </div>
                 </div>
-              </Card>
-              <Card>
+
+                {/* comments */}
                 {post && (
                 <Comments
                   context={process.env.PROJECT_CONTEXT || ''}
