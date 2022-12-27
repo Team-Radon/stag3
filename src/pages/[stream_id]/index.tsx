706df41a021f1330b55ad98521f8616adebbf118
Cristiana Chavez <cristianachavez214@gmail.com>
2022-12-28 03:24:42 +0800

    Link validation

--- src/pages/[stream_id]/index.tsx
+++ src/pages/[stream_id]/index.tsx
@@ -10,9 +10,9 @@ import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid';
 import { Comments } from '@/components/Comments';
 import { ButtonReaction } from '@/components/ButtonReaction';
 import { AvatarUser } from '@/components/AvatarUser';
+import { ProjectLogo } from '@/components/ProjectLogo';
 import { useGetProject } from '../../orbis/useGetProject';
 import { useUsername } from '../../hooks/useUsername';
-import { ProjectLogo } from '@/components/ProjectLogo';
 
 const Project = () => {
   const {
@@ -30,7 +30,7 @@ const Project = () => {
   if (error) {
     return <Error statusCode={404} />;
   }
-
+  
   return (
     <GridLayout className="pt-16">
       {!post && isLoading
@@ -54,9 +54,8 @@ const Project = () => {
                 <div className="flex flex-col md:flex-row justify-between items-start px-4 md:px-6 -mt-16 md:mt-12">
                   <div className="flex flex-col items-center md:flex-row gap-4 md:gap-6">
                     <div className="logo shrink-0 relative aspect-square w-24 rounded-md md:rounded-lg overflow-hidden">
-                   
 
-                      <ProjectLogo   className="w-full h-full" logo={post?.data?.content?.data?.logo} size="100"  />
+                      <ProjectLogo className="w-full h-full" logo={post?.data?.content?.data?.logo} size="100" />
                     </div>
                     <div className="px-4 md:px-0">
                       <div className="text-center md:text-left text-black">
@@ -98,29 +97,29 @@ const Project = () => {
                 {/* links */}
                 <div className="links flex flex-col md:flex-row md:gap-20 px-4 md:px-6 my-6">
                   <div className="official-sites md:w-1/3">
-                    <Link href={post?.data?.content?.data?.website ?? ''} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
+                    <Link href={post?.data?.content?.data?.website || '#'} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
                       <span>Website</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
-                    <Link href={post?.data?.content?.data?.whitepaper ?? ''} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
+                    <Link href={post?.data?.content?.data?.whitepaper || '#'} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
                       <span>Whitepaper / Litepaper</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
-                    <Link href={post?.data?.content?.data?.github ?? ''} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
+                    <Link href={'https://github.com/'+post?.data?.content?.data?.github || '#'} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
                       <span>Github</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
-                    <Link href={post?.data?.content?.data?.gitcoin ?? ''} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
+                    <Link href={'https://gitcoin.com/'+post?.data?.content?.data?.gitcoin || '#'} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
                       <span>Gitcoin</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
                   </div>
                   <div className="socials md:w-1/3 mt-4 md:mt-0">
-                    <Link href={post?.data?.content?.data?.discord ?? ''} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
+                    <Link href={'https://discord.com/'+post?.data?.content?.data?.discord || '#'} className="group flex items-center gap-1 text-sm text-sky-600 hover:underline">
                       <span>Discord</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
-                    <Link href={post?.data?.content?.data?.twitter ?? ''} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
+                    <Link href={'https://twitter.com/'+post?.data?.content?.data?.twitter || '#'} className="group flex items-center gap-1 mt-4 text-sm text-sky-600 hover:underline">
                       <span>Twitter</span>
                       <ArrowTopRightOnSquareIcon className="w-4 h-4 hidden group-hover:block" />
                     </Link>
