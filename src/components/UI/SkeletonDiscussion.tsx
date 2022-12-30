90cfef70a712d45647ed78f118b45e4118730613
ayad-nadeeb <nadeeb.ayad@gmail.com>
2022-12-31 03:00:22 +0800

    skeleton - discussion items

---
+++ src/components/UI/SkeletonDiscussion.tsx
@@ -0,0 +1,93 @@
+import Card from './Card';
+
+const SkeletonDiscussions = () => (
+  <Card>
+    <div className="animate-pulse">
+      <div className="divide-y divide-skin-divider">
+        <div className="flex items-center gap-4 px-4 md:px-6 py-10">
+          <div className="space-y-1 px-2 py-1">
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+            <div className="w-4 h-4 bg-gray-300 rounded" />
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+          </div>
+          <div className="shrink-0 w-8 h-8 bg-gray-300 rounded-full" />
+          <div className="w-full">
+            <div className="w-3/4 h-[28px] bg-gray-300 rounded-full" />
+            <div className="flex items-center gap-4 !text-xs mt-2">
+              <div className="w-16 h-5 bg-gray-300 rounded-lg" />
+              <div className="w-10 h-4 bg-gray-300 rounded-lg" />
+              <div className="w-16 h-4 bg-gray-300 rounded-lg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 px-4 md:px-6 py-10">
+          <div className="space-y-1 px-2 py-1">
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+            <div className="w-4 h-4 bg-gray-300 rounded" />
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+          </div>
+          <div className="shrink-0 w-8 h-8 bg-gray-300 rounded-full" />
+          <div className="w-full">
+            <div className="w-4/5 h-[28px] bg-gray-300 rounded-full" />
+            <div className="flex items-center gap-4 !text-xs mt-2">
+              <div className="w-16 h-5 bg-gray-300 rounded-lg" />
+              <div className="w-10 h-4 bg-gray-300 rounded-lg" />
+              <div className="w-16 h-4 bg-gray-300 rounded-lg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 px-4 md:px-6 py-10">
+          <div className="space-y-1 px-2 py-1">
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+            <div className="w-4 h-4 bg-gray-300 rounded" />
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+          </div>
+          <div className="shrink-0 w-8 h-8 bg-gray-300 rounded-full" />
+          <div className="w-full">
+            <div className="w-3/4 h-[28px] bg-gray-300 rounded-full" />
+            <div className="flex items-center gap-4 !text-xs mt-2">
+              <div className="w-16 h-5 bg-gray-300 rounded-lg" />
+              <div className="w-10 h-4 bg-gray-300 rounded-lg" />
+              <div className="w-16 h-4 bg-gray-300 rounded-lg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 px-4 md:px-6 py-10">
+          <div className="space-y-1 px-2 py-1">
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+            <div className="w-4 h-4 bg-gray-300 rounded" />
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+          </div>
+          <div className="shrink-0 w-8 h-8 bg-gray-300 rounded-full" />
+          <div className="w-full">
+            <div className="w-3/4 h-[28px] bg-gray-300 rounded-full" />
+            <div className="flex items-center gap-4 !text-xs mt-2">
+              <div className="w-16 h-5 bg-gray-300 rounded-lg" />
+              <div className="w-10 h-4 bg-gray-300 rounded-lg" />
+              <div className="w-16 h-4 bg-gray-300 rounded-lg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 px-4 md:px-6 py-10">
+          <div className="space-y-1 px-2 py-1">
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+            <div className="w-4 h-4 bg-gray-300 rounded" />
+            <div className="w-4 h-5 bg-gray-300 rounded" />
+          </div>
+          <div className="shrink-0 w-8 h-8 bg-gray-300 rounded-full" />
+          <div className="w-full">
+            <div className="w-4/5 h-[28px] bg-gray-300 rounded-full" />
+            <div className="flex items-center gap-4 !text-xs mt-2">
+              <div className="w-16 h-5 bg-gray-300 rounded-lg" />
+              <div className="w-10 h-4 bg-gray-300 rounded-lg" />
+              <div className="w-16 h-4 bg-gray-300 rounded-lg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  </Card>
+)
+
+export default SkeletonDiscussions;
