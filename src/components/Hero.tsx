bf1427dd1ae40bd76278877da36f43393ac5b03d
Cristiana Chavez <cristianachavez214@gmail.com>
2022-12-30 14:38:19 +0800

    add texts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <span className="text-indigo-600">Blockchain builders</span>
       </h1>
       <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg md:mt-5 md:text-xl">
-        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        Stag3 is where you present your web3 ideas and projects to fellow blockchain builders. Get specialized feedback and widen your network for future projects.
       </p>
       <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
         <div className="hidden md:flex h-8 items-center gap-3">
