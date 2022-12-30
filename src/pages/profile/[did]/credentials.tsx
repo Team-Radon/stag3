5f5d1412074d5e50fa0900a3f88df965462780d7
Cristiana Chavez <cristianachavez214@gmail.com>
2022-12-31 01:44:21 +0800

    credentials specific for users

--- src/pages/profile/[did]/credentials.tsx
+++ src/pages/profile/[did]/credentials.tsx
@@ -1,16 +1,22 @@
 
 import { GridItemEight, GridItemFour, GridLayout } from '@/components/GridLayout'
 import { ProfileHeader } from '@/components/ProfileHeader'
+import { useRouter } from 'next/router';
 import { GitCredentials } from '../../../components/GitCredentials'
 
-const ProfileCredentials = () => (
-  <GridLayout>
-    <GridItemFour>
-      <ProfileHeader />
-    </GridItemFour>
-    <GridItemEight>
-      <GitCredentials />
-    </GridItemEight>
-  </GridLayout>
-)
+const ProfileCredentials = () => {
+  const {
+    query: { did }
+  } = useRouter();
+  return (
+    <GridLayout>
+      <GridItemFour>
+        <ProfileHeader />
+      </GridItemFour>
+      <GridItemEight>
+        <GitCredentials did={did?.toString()} />
+      </GridItemEight>
+    </GridLayout>
+  )
+}
 export default ProfileCredentials
